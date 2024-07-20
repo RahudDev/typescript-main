e21ceca9e295baac7c2f03d6e90066e1473df75c
rahud <rahudnainggolan@gmail.com>
2024-07-20 11:44:13 +0700

    first commit

---
+++ dist/main.js
@@ -0,0 +1,5 @@
+"use strict";
+var button = document.getElementById('myButton');
+button === null || button === void 0 ? void 0 : button.addEventListener('click', function () {
+    alert('Hello, you clicked the button!');
+});
